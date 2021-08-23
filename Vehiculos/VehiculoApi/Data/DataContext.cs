0c077ade388a4ef5dc5b176d0d0f0f7e6bdc1ff9
Percibal Vasquez <vasquez.percibal@gmail.com>
2021-08-22 21:37:55 -0400

    adicionar vehiculostype

---
+++ Vehiculos/VehiculoApi/Data/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VehiculoApi.Data.Entities;
+
+namespace VehiculoApi.Data
+{
+    public class DataContext :DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) :base(options)
+        {
+        }
+        public DbSet<VehicleType> vehicleTypes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) //aca creamos una exprecion unica que no se repitan la descripciones 
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<VehicleType>().HasIndex(x => x.Description).IsUnique();//exprecioon lamda se indica indice unico
+        }
+       
+    }
+}
